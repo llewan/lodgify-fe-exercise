433fa8128ea803b51b451e7d315b0858d0eacda4
Leonardo Lewandowski <leonardo.lewandowski@mercadolibre.com>
2020-10-18 20:18:08 -0300

    Proyect structure and scaffolding

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  output: {
+    filename: 'app.[contentHash].js'
+  },
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.(js)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader',
+        ],
+      },
+    ]
+  },
+  plugins: [new HtmlWebpackPlugin({
+    template: 'src/index.html'
+  })]
+};
